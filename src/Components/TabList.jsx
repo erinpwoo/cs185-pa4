1824cc16a5ef588a607898b916e58e4eb01a7610
Erin <erinpwoo@yahoo.com>
2021-02-21 23:00:00 -0800

    tabs ui

--- src/Components/TabList.jsx
+++ src/Components/TabList.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react"
+
+const tabs = ["Text", "Image", "Video", "Table"];
+
 export const TabList = () => {
+    const [activeTab, setActiveTab] = useState(tabs[0])
     return <>
-        <h1>Tab list</h1>
-    </>
+        <div class="tabs">
+            {tabs.map((tab) => (
+                <button activeTab={activeTab === tab} onClick={() => setActiveTab(tab)}>
+                    {tab}
+                </button>
+            ))}
+        </div>
+    </>;
 }
